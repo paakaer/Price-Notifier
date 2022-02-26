cf4eb78a3bc777b432faab45bfec5a3de7d9a7f0
paakaer <kweku.ogoe@tuta.io>
2022-02-26 23:34:35 +0100

    can open browser; can open tabs and links; cleaned code

--- PriceNotifier/main.py
+++ PriceNotifier/main.py
@@ -10,23 +10,10 @@ def tabInitializer(tabHandler):
         driver.switch_to.new_window("tab")
 
 
-# print(fileData["Amazon"]["Amazon Italia"])
-
 with open("Stores.json", 'r') as file:
     fileData = json.load(file)
 
 driver = webdriver.Firefox(executable_path="/home/paakaer/PycharmProjects/PriceNotifier/drivers/geckodriver")
 tabInitializer(driver)
-'''originalWindow = driver.current_window_handle
-tabInitializer(originalWindow)
-driver.switch_to.new_window("tab")
-driver.get(fileData["Amazon"]["Amazon Italia"])
-for site in fileData["Ebay"]:
-    driver.switch_to.new_window("tab")
-    driver.get(site["url"])
-# driver.get("")
-# driver.switch_to.new_window('tab')
 
-tabInitializer(driver)
-'''
 driver.quit()
